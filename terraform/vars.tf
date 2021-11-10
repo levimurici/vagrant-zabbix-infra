fd0e70acc400d938cd9014769e1eb1008cd3bbee
levimurici <levi.murici@gmail.com>
2021-11-10 16:34:13 -0300

    Criando infra aws via terraform

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,8 +1,7 @@
 #Variáveis das imagens
 variable "amis" {
     type = "map"
-
     default = {
-        "ami_services" = "ami-083654bd07b5da81d"
+        "ami-services" = "ami-083654bd07b5da81d"
     }
 }
