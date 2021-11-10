c0c910c99d3fbed0c38d0265167c548d7c23755b
levimurici <levi.murici@gmail.com>
2021-11-10 11:26:57 -0300

    Iniciando configurações de infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-/* terraform {
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
-} */
+}
 
 # Configure the AWS Provider
 provider "aws" {
