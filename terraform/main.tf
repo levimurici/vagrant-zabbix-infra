a18da1cd9bc50a859abf3e7e64e7c2eb96081a2f
levimurici <levi.murici@gmail.com>
2021-11-10 11:45:05 -0300

    Iniciando configurações de infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,7 @@
-/* terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
- */
-
 # Configure the AWS Provider
 provider "aws" {
-    version = "~> 3.0"
-    region = "us-east-1"
+  version = "~> 3.0"
+  region  = "us-east-1"
 }
 
 //Instância que irá rodar os microserviços em docker-compose
