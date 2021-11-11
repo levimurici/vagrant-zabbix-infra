5a9d4e01aa0416731941f2caf3534883b9049106
levimurici <levi.murici@gmail.com>
2021-11-11 11:07:26 -0300

    Iniciando a construção da infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,28 @@ resource "aws_instance" "services" {
     }
     vpc_security_group_ids = ["${aws_security_group.acesso-dnd.id}"]
     depends_on = [aws_s3_bucket.dump-services]
+
+    provisioner "local-exec" {
+      working_dir = "/tmp"
+      command     = <<EOT
+        if ! ``docker --version > /dev/null 2>&1`` ; then 
+          sudo curl -sSL https://get.docker.com/ | sh;
+          sudo usermod -aG docker `echo $USER`;
+          sudo setfacl -m user:`echo $USER`:rw /var/run/docker.sock
+        fi
+    EOT
+  }
+}
+
+resource "aws_instance" "zabbix" {
+    count = 1
+    ami = var.amis["ami-services"] # Ubuntu 20.1 LTS x86
+    instance_type = "t2.micro"
+    key_name = var.key_name["keyname-services"]
+    tags = {
+      Name = "Zabbix-server"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-dnd.id}"]
 }
 
 resource "aws_s3_bucket" "dump-services" {
