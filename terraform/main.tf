edc0ce99ea6eaeed8782c7d6dbcfdc841ab46ccb
levimurici <levi.murici@gmail.com>
2021-11-11 12:22:08 -0300

    Iniciando a construção da infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,29 +4,6 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "null_resource" "install_docker" {
-  provisioner "local-exec" {
-    working_dir = "/tmp"
-    command     = <<EOT
-      if ! ``docker --version > /dev/null 2>&1`` ; then 
-        sudo curl -sSL https://get.docker.com/ | sh;
-        sudo usermod -aG docker `echo $USER`;
-        sudo setfacl -m user:`echo $USER`:rw /var/run/docker.sock
-      fi
-    sleep 3;
-    sudo apt-get install -y python3 python3-pip;
-    sudo apt install -y docker-compose;
-    sudo apt install -y git;
-    
-    sleep 3; 
-    cd ~;
-    git clone https://github.com/levimurici/suricato-iot && cd suricato-iot;
-    docker-compose up -d;
-    fi;
-   EOT
-  }
-}
-
 # Instância que irá rodar os microserviços em docker-compose
 resource "aws_instance" "services" {
     depends_on = [
@@ -42,16 +19,26 @@ resource "aws_instance" "services" {
     }
     vpc_security_group_ids = ["${aws_security_group.acesso-dnd.id}"]
 
-/*  provisioner "local-exec" {
-      working_dir = "/tmp"
-      command     = <<EOT
-        if ! ``docker --version > /dev/null 2>&1`` ; then 
-          sudo curl -sSL https://get.docker.com/ | sh;
-          sudo usermod -aG docker `echo $USER`;
-          sudo setfacl -m user:`echo $USER`:rw /var/run/docker.sock
-        fi
+  provisioner "local-exec" {
+    working_dir = "/tmp"
+    command     = <<EOT
+      if ! ``docker --version > /dev/null 2>&1`` ; then 
+        sudo curl -sSL https://get.docker.com/ | sh;
+        sudo usermod -aG docker `echo $USER`;
+        sudo setfacl -m user:`echo $USER`:rw /var/run/docker.sock
+      fi
+      sleep 3;
+      sudo apt-get install -y python3 python3-pip;
+      sudo apt install -y docker-compose;
+      sudo apt install -y git;
+      
+      sleep 3; 
+      cd ~;
+      git clone https://github.com/levimurici/suricato-iot && cd suricato-iot;
+      docker-compose up -d;
+      fi;
     EOT
-  } */
+  }
 }
 
 resource "aws_instance" "zabbix" {
