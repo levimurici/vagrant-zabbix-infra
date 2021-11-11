e662e8c35257802dcba20f8bb4f49a56c81a5093
levimurici <levi.murici@gmail.com>
2021-11-11 13:32:09 -0300

    Iniciando a construção da infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,7 +82,7 @@ resource "aws_vpc" "dnd_vpc" {
 resource "aws_subnet" "dnd_subnet" {
   vpc_id            = aws_vpc.dnd_vpc.id
   cidr_block        = "10.0.10.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "dnd_subnet"
