5755a654b75cb9ad1ea8e8e12e1c1fac133285a4
levimurici <levi.murici@gmail.com>
2021-11-11 13:55:13 -0300

    Iniciando a construção da infra

---
+++ terraform/network.tf
@@ -0,0 +1,28 @@
+
+# Create a VPC
+resource "aws_vpc" "dnd_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "dnd_vpc"
+  }
+}
+
+resource "aws_subnet" "dnd_subnet" {
+  vpc_id            = aws_vpc.dnd_vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "dnd_subnet"
+  }
+}
+
+resource "aws_network_interface" "interface-1" {
+  subnet_id   = aws_subnet.dnd_subnet.id
+  private_ips = ["10.0.10.100", "10.0.10.101"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
