8acf6c5d1dcb87d04dc515b8f47927cd086251af
levimurici <levi.murici@gmail.com>
2021-11-11 13:54:32 -0300

    Iniciando a construção da infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "services" {
       device_index         = 0
     }
 
-  provisioner "local-exec" {
+  /* provisioner "local-exec" {
     working_dir = "/tmp"
     command     = <<EOT
       if ! ``docker --version > /dev/null 2>&1`` ; then 
@@ -40,7 +40,7 @@ resource "aws_instance" "services" {
       docker-compose up -d;
       fi;
     EOT
-  }
+  } */
 }
 
 resource "aws_instance" "zabbix" {
@@ -67,33 +67,5 @@ resource "aws_s3_bucket" "dump-services" {
   tags = {
     Name        = "suricato-dump-services"
     Environment = "Dev"
-  }
-}
-
-# Create a VPC
-resource "aws_vpc" "dnd_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "dnd_vpc"
-  }
-}
-
-resource "aws_subnet" "dnd_subnet" {
-  vpc_id            = aws_vpc.dnd_vpc.id
-  cidr_block        = "10.0.10.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "dnd_subnet"
-  }
-}
-
-resource "aws_network_interface" "interface-1" {
-  subnet_id   = aws_subnet.dnd_subnet.id
-  private_ips = ["10.0.10.100", "10.0.10.101"]
-
-  tags = {
-    Name = "primary_network_interface"
   }
 }
