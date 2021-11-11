a9a0cecd014a0aed404bc1329715dbb0da8f6450
levimurici <levi.murici@gmail.com>
2021-11-11 11:59:06 -0300

    Iniciando a construção da infra

---
+++ terraform/application.tf
@@ -0,0 +1,26 @@
+resource "null_resource" "install_docker" {
+  provisioner "local-exec" {
+    working_dir = "/tmp"
+    command     = <<EOT
+      if ! ``docker --version > /dev/null 2>&1`` ; then 
+        sudo curl -sSL https://get.docker.com/ | sh;
+        sudo usermod -aG docker `echo $USER`;
+        sudo setfacl -m user:`echo $USER`:rw /var/run/docker.sock
+        sudo apt-get install -y python3 python3-pip;
+        sudo apt install -y docker-compose;
+      fi
+   EOT
+  }
+}
+
+resource "null_resource" "install_services" {
+  provisioner "local-exec" {
+    working_dir = "~/"
+    command     = <<EOT
+        sudo apt install -y git;
+        sleep 3;
+        git clone https://github.com/levimurici/suricato-iot && cd suricato-iot;
+        docker-compose up -d
+   EOT
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,11 @@ provider "aws" {
 
 # Instância que irá rodar os microserviços em docker-compose
 resource "aws_instance" "services" {
+    depends_on = [
+      aws_s3_bucket.dump-services, 
+      "null_resource.install_docker",
+      "null_resource.install_services"
+      ]
     count = 1
     ami = var.amis["ami-services"] # Ubuntu 20.1 LTS x86
     instance_type = "t2.micro"
@@ -14,9 +19,8 @@ resource "aws_instance" "services" {
       Name = "Serviços-${count.index}"
     }
     vpc_security_group_ids = ["${aws_security_group.acesso-dnd.id}"]
-    depends_on = [aws_s3_bucket.dump-services]
 
-/*     provisioner "local-exec" {
+/*  provisioner "local-exec" {
       working_dir = "/tmp"
       command     = <<EOT
         if ! ``docker --version > /dev/null 2>&1`` ; then 
